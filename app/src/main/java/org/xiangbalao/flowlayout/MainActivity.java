cddb48b51bd274de584ca2bf75a7f1eef7395eb5
longtaoge <61852263@qq.com>
2015-05-05 00:33:14 +0800

    添加截图

--- app/src/main/java/org/xiangbalao/flowlayout/MainActivity.java
+++ app/src/main/java/org/xiangbalao/flowlayout/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,6 +41,8 @@ public class MainActivity extends Activity implements OnClickListener {
 		super.onCreate(savedInstanceState);
 		//设置无标题
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
 
 		init();
